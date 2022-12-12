fe8243959d66c39a17a35f54de96023c172c3b62
Sohag <sohaghossain7th@gmail.com>
2022-12-12 18:16:02 +0600

    update

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,9 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div >
-
-      <h1>This is Main Page</h1>
       <CarrosulHome />
       <CategoryHome />
+      <HotelContainer />
 
 
     </div>
